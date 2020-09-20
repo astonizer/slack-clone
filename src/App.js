54c86eb9a1b8e22f8f6121312ea70bddd3865b22
Astel <astelthottankara@gmail.com>
2020-09-20 17:25:03 +0530

    Added chat routing UI

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Header from './components/header/Header';
 import './App.css'
 import Sidebar from './components/sidebar/Sidebar';
+import Chat from './components/sidebar/chat/Chat'
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Sidebar />
-      {/* Routing */}
+      <BrowserRouter>
+        <Header />
+        <div className="app_body">
+          <Sidebar>
+          <Switch>
+            <Route path="channel/:channelId">
+              <Chat />
+            </Route>
+            <Route path="/">
+              <h1>Welcome</h1>
+            </Route>
+          </Switch>
+          </Sidebar>
+        </div>
+        {/* Routing */}
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/sidebar/chat/Chat.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../../styles/Chat.css'
+
+function Chat() {
+    return (
+        <div className="chat">
+            Chat Screen
+        </div>
+    )
+}
+
+export default Chat
