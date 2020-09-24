c3ab54865752b0866e425dbb010099519353d074
Astel <astelthottankara@gmail.com>
2020-09-24 21:02:38 +0530

    Added create new channel feature

--- src/components/sidebar/SidebarOption.js
+++ src/components/sidebar/SidebarOption.js
@@ -1,9 +1,29 @@
 import React from 'react'
 import '../styles/SidebarOption.css'
+import { useHistory } from 'react-router-dom'
+import db from '../../fbConfig'
 
-function SidebarOption({ Icon, title }) {
+function SidebarOption({ Icon, title, id, addChannelOption }) {
+    const history = useHistory()
+
+    const selectChannel = () => {
+        if(id) {
+            history.push(`/channels/${id}`)
+        } else {
+            history.push(title)
+        }
+    }
+
+    const addChannel = () => {
+        let channelName = prompt("Please enter new channel name")
+        if(channelName) {
+            db.collection('channels').add({
+                name: channelName
+            })
+        }
+    }
     return (
-        <div className="sidebarOption">
+        <div className="sidebarOption" onClick={addChannelOption ? addChannel : selectChannel} >
             { Icon && <Icon className="sidebarOption_icon"/> }
             { Icon ? (
                 <h3>
